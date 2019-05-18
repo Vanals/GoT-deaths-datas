2afad1c605bbaafa04aecfa84842a6b97b50f6e0
Vanals <marco.vanali@vodafone.com>
2019-05-18 17:45:24 +0100

    Created basic javascript bundle webserver

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(process.cwd(), 'dist'),
+    compress: true,
+    port: 8050
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
